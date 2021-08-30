cbfb231fd205545bffa650807c7df5f98c3ea251
marwa <marwajawad19@gmail.com>
2021-08-30 17:41:30 -0400

    create todo form

--- src/features/todos/CreateTodo.js
+++ src/features/todos/CreateTodo.js
@@ -1,7 +1,18 @@
 import React from "react";
+import React from "react";
 
 function CreateTodo() {
-  return <div>Create Todo Component</div>;
+  return (
+    <div>
+      <form>
+        <p>
+          <label>add todo</label>
+          <input type="text" />
+        </p>
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
 
 export default CreateTodo;

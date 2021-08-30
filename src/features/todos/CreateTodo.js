62235288f71f1610b165a0c6d8d90a5900705fdb
marwa <marwajawad19@gmail.com>
2021-08-30 18:07:25 -0400

    adding handlesubmit function

--- src/features/todos/CreateTodo.js
+++ src/features/todos/CreateTodo.js
@@ -1,15 +1,21 @@
 import React from "react";
 import React from "react";
-
+import {useDispatch} from "react-redux"
 function CreateTodo() {
+  const dispatch =useDispatch()
   const [text, setText] = useState("");
 
   function handleChange(event) {
     setText(event.target.value);
   }
+   function handleSubmit(event) {
+    event.preventDefault();
+    dispatch(todoAdded(text));
+  }
   return (
+
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <p>
           <label>add todo</label>
           <input type="text" value={text} onChange={e=>handleChange(e)} />

7ed51db4aa20e41c69641e22f3746732d966e528
marwa <marwajawad19@gmail.com>
2021-08-30 17:57:29 -0400

    adding handle change for the input

--- src/features/todos/CreateTodo.js
+++ src/features/todos/CreateTodo.js
@@ -2,12 +2,17 @@ import React from "react";
 import React from "react";
 
 function CreateTodo() {
+  const [text, setText] = useState("");
+
+  function handleChange(event) {
+    setText(event.target.value);
+  }
   return (
     <div>
       <form>
         <p>
           <label>add todo</label>
-          <input type="text" />
+          <input type="text" onChange={e=>handleChange(e)} />
         </p>
         <input type="submit" />
       </form>
